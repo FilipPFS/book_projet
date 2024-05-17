763c0d993b0c1bf11786c79c9d0b490c0828c10b
Filip Petrovic <filippetrovicfr04@gmail.com>
2024-05-17 19:05:16 +0200

    Initialisation du projet + Etape 1.1 et 1.2

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((err) => console.log('Connexion à MongoDB échouée !', err));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.get("/", (req, res, next) => {
+    res.json({message: "Running on port 4000"});
+})
+
+
+module.exports = app;
